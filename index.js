baac84ce5bbdbce9a30cb1534840deac6530d408
Michael Fragiskatos <michael.fragiskatos@gmail.com>
2020-01-16 19:45:00 -0500

    remove authentication logic for movies

--- index.js
+++ index.js
@@ -42,28 +42,28 @@ app.get("/", function(req, res) {
 });
 
 //GET list of data about all movies
-app.get("/movies", passport.authenticate("jwt", { session: false }), function(
-  req,
-  res
-) {
+app.get("/movies", function(req, res) {
   Movies.find()
     .then(function(movies) {
       res.status(201).json(movies);
     })
-    .catch(function(error) {
-      console.error(error);
-      res.status(500).send("Error: " + error);
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
     });
 });
 
-// app.get("/movies", function(req, res) {
+// app.get("/movies", passport.authenticate("jwt", { session: false }), function(
+//   req,
+//   res
+// ) {
 //   Movies.find()
 //     .then(function(movies) {
 //       res.status(201).json(movies);
 //     })
-//     .catch(function(err) {
-//       console.error(err);
-//       res.status(500).send("Error: " + err);
+//     .catch(function(error) {
+//       console.error(error);
+//       res.status(500).send("Error: " + error);
 //     });
 // });
 
