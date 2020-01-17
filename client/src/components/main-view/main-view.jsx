ae8ccf72115510db484e225f26fed7ba3640f9a9
Michael Fragiskatos <michael.fragiskatos@gmail.com>
2020-01-16 20:47:06 -0500

    add movies endpoint

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -15,7 +15,17 @@ export class MainView extends React.Component {
   }
 
   componentDidMount() {
-    /* ... */
+    axios
+      .get("<https://movieswithmichaelf.herokuapp.com/movies>")
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   onMovieClick(movie) {
@@ -25,24 +35,20 @@ export class MainView extends React.Component {
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    // If the state isn't initialized, this will throw on runtime
+    // before the data is initially loaded
+    const { movies } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        {selectedMovie ? (
-          <MovieView movie={selectedMovie} />
-        ) : (
-          movies.map(movie => (
-            <MovieCard
-              key={movie._id}
-              movie={movie}
-              onClick={movie => this.onMovieClick(movie)}
-            />
-          ))
-        )}
+        {movies.map(movie => (
+          <div className="movie-card" key={movie._id}>
+            {movie.Title}
+          </div>
+        ))}
       </div>
     );
   }
