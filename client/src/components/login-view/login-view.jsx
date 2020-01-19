159a6ffc6ef3e9042a018dbfc51d2729a0e6a17a
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-01-19 11:31:17 -0500

    add form to login

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,27 +1,43 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     console.log(username, password);
-    // Send a request to the server for authentication then call props.onLoggedIn(username)
+  // Send a request to the server for authentication then call props.onLoggedIn(username)
     props.onLoggedIn(username);
-  };
+};
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="button" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Form>
+  <Form.Group controlId="formUserName">
+    <Form.Label>Username</Form.Label>
+    <Form.Control type="text" placeholder="Enter username" />
+    <Form.Control.Feedback type="invalid">
+                Please choose a username.
+              </Form.Control.Feedback>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
   );
 }
+
+LoginView.propTypes = {
+  onLoggedIn: PropTypes.func.isRequired
+};
