da6108d21864598251014fbc32b93e5a23b261b6
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-01-19 11:53:49 -0500

    add form to registration view

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,40 +1,41 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./login-view.scss";
 
 export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     console.log(username, password);
-  // Send a request to the server for authentication then call props.onLoggedIn(username)
+    // Send a request to the server for authentication then call props.onLoggedIn(username)
     props.onLoggedIn(username);
-};
+  };
 
   return (
     <Form>
-  <Form.Group controlId="formUserName">
-    <Form.Label>Username</Form.Label>
-    <Form.Control type="text" placeholder="Enter username" />
-    <Form.Control.Feedback type="invalid">
-                Please choose a username.
-              </Form.Control.Feedback>
-  </Form.Group>
+      <Form.Group controlId="formUserName">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Enter username" />
+        <Form.Control.Feedback type="invalid">
+          Please choose a username.
+        </Form.Control.Feedback>
+      </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -1,19 +1,62 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 // imports for files to bundle
-import './registration-view.scss';
+import "./registration-view.scss";
 
 export function RegistrationView(props) {
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const [ email, setEmail ] = useState('');
-    const [ birthday, setBirthday ] = useState('');
-}
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
 
-    RegistrationView.propTypes = {
-        onNewUserRegistered: PropTypes.func.isRequired
-      };
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(username, password);
+    // Send a request to the server for authentication then call props.onLoggedIn(username)
+    props.onLoggedIn(username);
+  };
+
+  return (
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicUserName">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Enter username" />
+        <Form.Control.Feedback type="invalid">
+          Please choose a username.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicDob">
+        <Form.Label>Date of Birth</Form.Label>
+        <Form.Control type="date" placeholder="01/01/1990" value={birthday} />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="I am not a robot" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+LoginView.propTypes = {
+  onLoggedIn: PropTypes.func.isRequired
+};
