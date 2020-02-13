4c45f36e3ab116ee966a3725fffb49b5cc880671
Michael Fragiskatos <michael.fragiskatos@gmail.com>
2020-02-12 22:35:33 -0500

    push heroku

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -130,7 +130,7 @@ export class ProfileView extends React.Component {
                           }
                         >
                           Delete
-                        </Button>
+                        </Button>                        
                       </li>
                     ))}
                   </ul>
